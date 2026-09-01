8fd13b4ff24e62a9702f579ed5e7ea8312bbe3ad
windy003 <928920471@qq.com>
2026-09-01 17:56:54 +0800

    调整

---
+++ Restart.ps1
@@ -0,0 +1,4 @@
+"D:\files\using\Python\P_Check_Website_Availability\Close.ps1"
+"D:\files\using\Python\P_Check_Website_Availability\Start.ps1"
+
+pause
---
+++ Start.ps1
@@ -0,0 +1,5 @@
+Set-Location "D:\files\using\Python\P_Check_Website_Availability"
+& "D:\files\using\Python\P_Check_Website_Availability\.venv\Scripts\pythonw.exe"  "D:\files\using\Python\P_Check_Website_Availability\monitor.py"
+
+
+pause
